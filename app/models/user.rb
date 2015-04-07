568cd9f7ad508199abc00e2c0ca9ecc4e7403a38
bruceliator <alexandr.tsibulko@gmail.com>
2015-04-07 23:10:00 +0300

    added sign-in-out functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  before_create :create_remember_token
   before_save { self.email = email.downcase }
   validates :name, length: { maximum: 50 }, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +8,19 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
 
+  class << self
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+  end
+
+  private
+
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 end
