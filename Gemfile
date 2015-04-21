3354d08e140201743a0301bdba856f8bd155037e
bruceliator <alexandr.tsibulko@gmail.com>
2015-04-21 20:55:35 +0300

    added updating users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '1.9.3'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'bootstrap-will_paginate', '0.0.9'
+gem 'twitter-bootstrap-rails-confirm'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-
 describe "User pages" do
 
   subject { page }
@@ -12,29 +11,47 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
 
-    describe "signup page" do
+  # describe "signup page" do
 
-    before { visit signup_path }
+  #   before { visit signup_path }
 
-    let(:submit) { "Create my account" }
+  #   let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
+  #   describe "with invalid information" do
+  #     it "should not create a user" do
+  #       expect { click_button submit }.not_to change(User, :count)
+  #     end
+  #   end
+
+  #   describe "with valid information" do
+  #     before do
+  #       fill_in "Name",         with: "Example User"
+  #       fill_in "Email",        with: "user@example.com"
+  #       fill_in "Password",     with: "foobar"
+  #       fill_in "Confirmation", with: "foobar"
+  #     end
+
+  #     it "should create a user" do
+  #       expect { click_button submit }.to change(User, :count).by(1)
+  #     end
+  #   end
+  # end
+
+  describe 'edit' do
+    let(:user) { FactoryGirl.create(:user) }
+    before {
+      visit edit_user_path(user) }
+
+    describe 'page' do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
     end
 
-    describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+    describe 'with invalid information' do
+      before { click_button "Save changes" }
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
-      end
+      it { should have_content('error') }
     end
   end
 end
